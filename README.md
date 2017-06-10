a9aa3bf044041ee709e70860132bc48b07221dc4
xuqiang521 <1257875587@qq.com>
2017-06-11 01:55:04 +0800

    version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Xue
+
+## 介绍
+
+一个属于自己的简易版本的mvvm框架，正在不断的完善中
+
+## 项目启动
+
+```bash
+# 安装依赖
+npm install
+
+# 本地服务
+npm run dev
+
+# 文件打包 (生成环境)
+npm run build
+
+# 文件打包 (本地打包)
+npm run build:local
+
+```
+
+## 进展
+
+- [x] 对象监听
+- [x] 数组监听
+- [ ] computed
+- [x] watch deep
+- [x] x-html
+- [x] x-text
+- [x] x-model
+- [ ] x-if
+- [ ] x-else
+- [ ] x-for
+- [x] x-on
+- [ ] Virtual Dom && diff
+- [ ] components
+
+## 总结
+
+目前先就着这些功能慢慢进行完善，拓展
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const webpack = require('webpack')
+const OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const port = process.env.NODE_ENV || 9000;
+
+const uri = "http://localhost:" + port;
+
+const ExtractLess = new ExtractTextPlugin({
+  filename: 'theme/[name].css'
+})
+
+module.exports = {
+  entry: {
+    xue: './src/modules/global-api.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          attrs: ['img:src', 'link:href']
+        }
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        // loader: 'style-loader!css-loader!less-loader',
+        loader: ExtractLess.extract(
+          {
+            fallback:'style-loader',
+            use: [
+              'css-loader',
+              'less-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: [require('autoprefixer')]
+                }
+              }
+            ]
+        })
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [
+    new OpenBrowserWebpackPlugin({
+      url: uri
+    }),
+    new HtmlWebpackPlugin({
+      // filename: 'pages/index.html',
+      template: './src/pages/index.html'
+    }),
+    // 生成独立css文件
+    ExtractLess
+    // new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.less'],
+    alias: {
+      'utils': path.resolve(__dirname, 'src/modules/utils'),
+      'compiler': path.resolve(__dirname, 'src/modules/compiler'),
+      'watcher': path.resolve(__dirname, 'src/modules/watcher'),
+      'observer': path.resolve(__dirname, 'src/modules/observer'),
+      'styles': path.resolve(__dirname, 'src/views')
+    }
+  },
+
+  devServer: {
+    // contentBase: path.resolve(__dirname, 'src'),
+    // hot: true,
+    port: port,
+    historyApiFallback: true
+  }
+}
