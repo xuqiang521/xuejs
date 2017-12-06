6eb309282cb64a44419de3bb1c851cd052a30c85
xuqiang <1257875587@qq.com>
2017-12-07 00:46:47 +0800

    ⚡️ optimize the parse of expression

--- src/modules/compiler/index.js
+++ src/modules/compiler/index.js
@@ -30,7 +30,7 @@ class Compiler {
     [].slice.call(childNodes).forEach(node => {
       let text = node.textContent;
       let reg = /\{\{((?:.|\n)+?)\}\}/g;
-      let match;
+      // let match;
 
       // 如果是element节点
       if (self.isElementNode(node)) {
@@ -38,11 +38,13 @@ class Compiler {
       }
       // 如果是text节点
       else if (self.isTextNode(node)) {
-        while ((match = reg.exec(text)) !== null) {
-          // console.log(match[1]);
-          
-          self.compileText(node, match[1])
-        }
+        // while ((match = reg.exec(text)) !== null) {
+        //   // console.log(match[1]);
+        //   self.compileText(node, match[1])
+        // }
+        text.replace(reg, (all, item) => {
+          self.compileText(node, item)
+        })
         // console.log(text.match(reg));
         // console.log(RegExp.$1);
         // self.compileText(node, RegExp.$1)
